513572bcad94a6da1d9f1be7163499c3b11aa308
= <=>
2023-07-12 10:42:12 -0300

    Game seed - inserido dados na tabela tb_game

--- src/main/java/com/dimassi/dslist/entities/Game.java
+++ src/main/java/com/dimassi/dslist/entities/Game.java
@@ -24,9 +24,14 @@ public class Game {
 	private Integer year;
 	
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")                //para aceitar mais 255 caracteres
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")	
 	private String longDescription;
 	
 	public Game() {
@@ -34,13 +39,14 @@ public class Game {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {	
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score =  score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
@@ -78,12 +84,21 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+	
+	
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
